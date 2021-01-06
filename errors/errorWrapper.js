14c4f3c6a6f76313fa141ad9d80ebe44fb3209cf
Jun4928 <jun.choi.4928@gmail.com>
2021-01-06 19:27:53 +0900

    Modify: getQueryOptions

--- errors/errorWrapper.js
+++ errors/errorWrapper.js
@@ -4,7 +4,6 @@ const errorGenerator = require('./errorGenerator')
 const errorWrapper = (controller) => async (req, res, next) => {
   try {
     const errors = validationResult(req)
-    console.log(errors)
     if (!errors.isEmpty()) errorGenerator({ statusCode: 400 })
 
     await controller(req, res, next)
